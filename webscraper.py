ed426709e54dabfcf58143463b9252bf04229158
Mohini Vembu <mohini.vem@gmail.com>
2023-12-13 20:03:03 +0530

    extract the raw data

--- webscraper.py
+++ webscraper.py
@@ -3,35 +3,46 @@ from bs4 import BeautifulSoup
 import pandas as pd
 from selenium import webdriver
 import pathlib
+import requests
 
 
 
-### Set up the initial connection
+# ### Set up the initial connection
 url = 'https://randomwalksbooth.org'
 driver = webdriver.Firefox()
 driver.get(url)
-
-### Go through each location
-n_trips = 0
+headers = {
+     'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10.12; rv:55.0) Gecko/20100101 Firefox/55.0',
+}
+# ### Go through each location
 locations = driver.find_elements('tag name', 'a')
-for location in locations:
+n_trips = 0
+trips = []
+raw_data = []
+for index, location in enumerate(locations):
     link = location.get_attribute('href')
     # exclude the email address + top-line links + family RW page
     if 'private' not in link:
         continue
     n_trips += 1
+    # click on the link
     driver.get(link)
+    result = requests.get(link, headers= headers).text
+    # scrape the relevant data
+    soup = BeautifulSoup(result, features= 'html.parser')
+    trip_leader_overall = soup.findChildren('table')[1]
+    each_leader_info = trip_leader_overall.findChildren('tr')[1:]
+    for row in each_leader_info:
+        temp = []
+        cells = row.findChildren('p')
+        for cell in cells:
+            value = cell.string
+            if value is None:
+                continue
+            else:
+                trips.append(link)
+                temp.append(value)
+        raw_data.append(','.join(temp))
     driver.back()
 
 driver.quit()
-print(n_trips)
-
-# soup = BeautifulSoup(raw_html, features= 'html.parser')
-# print(soup)
-
-
-# Scrape the data
-
-
-
-# Format the data
