0fb9fcb7c593e6ea175dc4d6367b9195a25cc9e1
Mohini Vembu <mohini.vem@gmail.com>
2023-12-14 16:55:52 +0530

    make into pandas df and export

--- webscraper.py
+++ webscraper.py
@@ -7,25 +7,27 @@ import requests
 
 
 
-# ### Set up the initial connection
+#### Set up the initial connection
 url = 'https://randomwalksbooth.org'
 driver = webdriver.Firefox()
 driver.get(url)
 headers = {
      'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10.12; rv:55.0) Gecko/20100101 Firefox/55.0',
 }
-# ### Go through each location
+
+#### Go through each location and add to Raw Data
 locations = driver.find_elements('tag name', 'a')
 n_trips = 0
-trips = []
-raw_data = []
+raw_data_frame = pd.DataFrame(columns= ['rw_destination', 'leader_data_raw'])
+
+
 for index, location in enumerate(locations):
     link = location.get_attribute('href')
     # exclude the email address + top-line links + family RW page
     if 'private' not in link:
         continue
     n_trips += 1
-    # click on the link
+    # click on the link and get the HTML of that page
     driver.get(link)
     result = requests.get(link, headers= headers).text
     # scrape the relevant data
@@ -40,9 +42,14 @@ for index, location in enumerate(locations):
             if value is None:
                 continue
             else:
-                trips.append(link)
                 temp.append(value)
-        raw_data.append(','.join(temp))
+        raw_data_frame.loc[raw_data_frame.shape[0]] = [location, ','.join(temp)]
     driver.back()
 
 driver.quit()
+
+# Format the data
+
+
+# Export the data
+raw_data_frame.to_csv('raw_uncleaned_data.csv')
