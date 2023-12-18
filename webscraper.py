3e0147ddad0db9ba38d26315ae124aaf312d7a82
Mohini Vembu <mohini.vem@gmail.com>
2023-12-18 10:18:20 +0530

    some logic fixes

--- webscraper.py
+++ webscraper.py
@@ -4,8 +4,7 @@ import pandas as pd
 from selenium import webdriver
 import pathlib
 import requests
-
-
+import re
 
 #### Set up the initial connection
 url = 'https://randomwalksbooth.org'
@@ -17,8 +16,9 @@ headers = {
 
 #### Go through each location and add to Raw Data
 locations = driver.find_elements('tag name', 'a')
-n_trips = 0
-raw_data_frame = pd.DataFrame(columns= ['rw_destination', 'leader_data_raw'])
+raw_data_frame = pd.DataFrame(columns= ['rw_destination','rw_destination2' ,'email', 'hometown', 'undergrad_college'
+                                        , 'undergrad_major', 'work_experience', 'booth_concentrations'
+                                        , 'random_walk_1y', 'hobbies', 'fun_facts'])
 
 
 for index, location in enumerate(locations):
@@ -26,16 +26,19 @@ for index, location in enumerate(locations):
     # exclude the email address + top-line links + family RW page
     if 'private' not in link:
         continue
-    n_trips += 1
+    # clean up the link to extract the location
+    clean_location = (re.findall(r'int\-(.+?)\.htm',link) or re.findall(r'dom\-(.+?)\.htm',link))[0]
     # click on the link and get the HTML of that page
     driver.get(link)
     result = requests.get(link, headers= headers).text
-    # scrape the relevant data
+    # scrape the relevant data - the second table on the page has the trip leader data
+    # within that table all after the first row contain data about a particular leader
     soup = BeautifulSoup(result, features= 'html.parser')
     trip_leader_overall = soup.findChildren('table')[1]
     each_leader_info = trip_leader_overall.findChildren('tr')[1:]
+    # go through each leader and export details (stored in p)
     for row in each_leader_info:
-        temp = []
+        temp = [location, clean_location]
         cells = row.findChildren('p')
         for cell in cells:
             value = cell.string
@@ -43,12 +46,18 @@ for index, location in enumerate(locations):
                 continue
             else:
                 temp.append(value)
-        raw_data_frame.loc[raw_data_frame.shape[0]] = [location, ','.join(temp)]
+        # add final data to dataframe
+        # for initial version, skip leaders who don't have complete data
+        try:
+            raw_data_frame.loc[raw_data_frame.shape[0]] = temp
+        except ValueError:
+            print('skipping leader, missing information')
     driver.back()
 
 driver.quit()
 
-# Format the data
+
+# Clean the data
 
 
 # Export the data
